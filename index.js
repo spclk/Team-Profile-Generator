ca741f9035064bb332ef4ef86ff73729b85a3520
Konstantin <konstantin1020@gmail.com>
2021-03-04 16:26:15 -0800

    functions and conditionals

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateHTML = require('./')
+const generateHTML = require('./src/generateHTML')
+const Manager = require ('./lib/Manager')
 
 var allEmplyees = [];
 
@@ -21,7 +22,7 @@ const questions = [
     message: "Please enter the employee's email:",
   },
   {
-    type: "input",
+    type: "list",
     name: "role",
     message: "Please enter the employee's role:",
     choices: ['Manager', 'Engineer', 'Intern']
@@ -35,9 +36,67 @@ function writeToFile(fileName, data) {
 function init() { 
     inquirer.prompt(questions)
     .then((answers) => {
-        var response = generateHTML(answers);
-        writeToFile("index.html", response)
+      console.log(answers)
+      if (answers.role === "Manager"){
+        inquirer.prompt({
+          type: "input",
+          name: "officeNumber",
+          message: "Please enter the manager's office number:",
+        })
+        .then((managerAnswers) => {
+          let newManager = new Manager(answers.name, answers.id, answers.email, managerAnswers.officeNumber) 
+          allEmplyees.push(newManager);
+          console.log(allEmplyees);
+          addAnother()
+        })
+      }
+      else if (answers.role === "Engineer"){
+        inquirer.prompt({
+          type: "input",
+          name: "gitHub",
+          message: "Please enter the Engineer's GitHub user name:",
+        })
+        .then((engineerAnswers) => {
+          let newManager = new Engineer(answers.name, answers.id, answers.email, managerAnswers.officeNumber) 
+          allEmplyees.push(newManager);
+          console.log(allEmplyees);
+        })
+      }
+      else if (answers.role === "Intern"){
+        inquirer.prompt({
+          type: "input",
+          name: "school",
+          message: "Please enter the name of the school for the Intern:",
+        })
+        .then((internAnswers) => {
+          let newManager = new Manager(answers.name, answers.id, answers.email, managerAnswers.officeNumber) 
+          allEmplyees.push(newManager);
+          console.log(allEmplyees);
+        })
+      }
+      
+      
     })
+
+}
+
+
+function addAnother() {
+  inquirer.prompt({
+    type: "confirm",
+    name: "newEmployee",
+    message: "Would you like to add another employee?",
+  })
+  .then((anotherEmployee) => {
+  if (anotherEmployee.newEmployee){
+    init();
+  }
+  else {
+    //var response = generateHTML(answers);
+  //writeToFile("index.html", response)
+  }
+}) 
+
 
 }
 init();
